0e8d5d29fea9fa2c204a54d3f319c0a6db12c3fa
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-08-30 19:43:23 -0700

    Basic Commands Noted
    Basic inputs noted.

--- custom_calculator.py
+++ custom_calculator.py
@@ -2,7 +2,15 @@
 # based on their budget and the number of monthly payments they'd like to make at
 # 0% interest to pay it off.
 
+# Formula to calculate monthly payments: Monthly Payments = {budget} / {installments}
+
 print("Welcome to Arj's Custom Monthly Ferrari Payment Calculator!")
 
-name = 'Arj'
+input("What's your name?")
+
+response = input("What's your name?")
+
+budget = input("What's your Ferrari budget? Minimum is $100,000!")
+
+installments = input("How many monthly installments would you like to make?")
 
