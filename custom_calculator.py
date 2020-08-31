cc9b101844e6cf2f306d7b0e0377cb679157440a
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-08-30 17:39:19 -0700

    Initial Idea
    Custom Guitar Calculator initial idea.

---
+++ custom_calculator.py
@@ -0,0 +1,7 @@
+# This custom calculator will help aspiring guitarists choose which type of guitar
+# would be best for them based on the type of music they like.
+
+# Some recommendations are as follows:
+# "Fender Stratocaster" for classic rock and blues, like Eric Clapton and Jimi Hendrix
+# "Fender Telecaster" for country
+# "Gibson Les Paul" for hard rock, like Led Zeppelin and Guns 'N Roses
