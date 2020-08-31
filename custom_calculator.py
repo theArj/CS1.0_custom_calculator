4df9e6be0bdc42789e3fd92dc8a0d49b38cb69b3
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-08-30 20:08:29 -0700

    Foundational Coding Now Works
    The output correctly calculates the monthly payment. However, I am trying to see if there is a way to restrict the user input of budget to $100,000.

--- custom_calculator.py
+++ custom_calculator.py
@@ -1,4 +1,4 @@
-# This custom calculator provides the amount of monthly payments for a product
+# This custom calculator provides the amount of monthly payments for a Ferrari
 # based on their budget and the number of monthly payments they'd like to make at
 # 0% interest to pay it off.
 
@@ -6,11 +6,12 @@
 
 print("Welcome to Arj's Custom Monthly Ferrari Payment Calculator!")
 
-input("What's your name?")
-
-response = input("What's your name?")
+name = input("What's your name?")
 
 budget = input("What's your Ferrari budget? Minimum is $100,000!")
 
 installments = input("How many monthly installments would you like to make?")
 
+monthly_cost = int(budget)/int(installments)
+
+print(f"{name}'s monthly Ferrari payment is ${monthly_cost}.")
