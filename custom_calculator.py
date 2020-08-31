bdf55d4fd8b7c9ccfd459c6f813eb2e7eee5e66c
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-08-30 19:30:47 -0700

    Idea Change
    After Slacking with instructor, Joi, she recommended the monthly payment calculator, instead.
    
    Deleted previous ideas after saving to Notes file.

--- custom_calculator.py
+++ custom_calculator.py
@@ -1,45 +1,8 @@
-# This custom calculator will help aspiring guitarists choose which type of guitar
-# would be best for them based on the type of music they like.
+# This custom calculator provides the amount of monthly payments for a product
+# based on their budget and the number of monthly payments they'd like to make at
+# 0% interest to pay it off.
 
-# Some recommendations are as follows:
-# "Fender Stratocaster" for classic rock and blues, like Eric Clapton and Jimi Hendrix
-# "Fender Telecaster" for country
-# "Gibson Les Paul" for hard rock, like Led Zeppelin and Guns 'N Roses
+print("Welcome to Arj's Custom Monthly Ferrari Payment Calculator!")
 
-# Alternatively, write a program that gives 4 choices of artists numbered 1 - 4, and
-# assign a guitar to each artist. Based on the artist choice by entering the respective
-# number, the result will be the appropriate guitar.
-
-# Scenario: You love music and have dreams of being a rockstar! But you don't know where to start.
-# Enter, the Custom Guitar Calculator! Simply, you are given several choices of world famous
-# guitar players in different genres to choose your favorite. Based on your favorite choice, the
-# Custom Guitar Calculator will provide you with the perfect options for you.
-
-# <--- Include an print statement welcoming the user to the Custom Guitar Calculator and asking for their name. --->
-
-# <--- Upon user entering their name, greet them with a friendly greeting using the username, then ask
-#   the user to choose from the list of 4 options printed on the screen:
-#       1. Eric Clapton/Jimi Hendrix: Fender Stratocaster
-#       2. Brad Paisley/Keith Urban: Fender Telecaster
-#       3. Led Zeppelin/Guns N' Roses: Gibson Les Paul
-#       4. Kirk Hammett/Joe Satriani: ESP or Ibanez
-
-# Rubric: Input - Prompt the user to enter at least two numeric values that will be used in the calculation.
-
-# Perhaps, assign a number to a genre and assign a number to an artist from that genre.
-# Here are some genre and choices within those genres as examples:
-#       1. Rock/Blues
-#           1. Eric Clapton: Fender Stratocaster
-#           2. Jimi Page: Gibson Les Paul
-#       2. Country
-#           1. Keith Urban: Fender Telecaster
-#           2. Brad Paisley: Fender Telecaster
-#       3. Jazz
-#           1. Les Paul: Gibson Les Paul
-#           2. Wes Montgomery: Gibson L5-CES
-
-# Because two numerical values are needed, a second alternative idea is to calculate the monthly payments for
-# a product based on their budget and the number of monthly payments they'd like to make at 0% interest to
-# pay it off.
-
+name = 'Arj'
 
